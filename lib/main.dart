2c03b579f1982120720e618291fbe80bff183bcf
andreshg <andreshg@viridian.ltd>
2018-12-13 17:58:09 -0400

    removed unnecessary packages, updated to latest package version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:android_multiple_identifier/android_multiple_identifier.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:flutter/services.dart';
-
-import 'package:simple_permissions/simple_permissions.dart';
-import 'utils/encoder.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +15,6 @@ class _MyAppState extends State<MyApp> {
   String _imei = 'Unknown';
   String _serial = 'Unknown';
   String _androidID = 'Unknown';
-  String _androidIDChar = '00';
 
   @override
   void initState() {
@@ -34,8 +28,6 @@ class _MyAppState extends State<MyApp> {
     String imei;
     String serial;
     String androidID;
-    String androidIDChar;
-    // final res = await SimplePermissions.requestPermission(Permission.ReadPhoneState);
 
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
@@ -43,27 +35,13 @@ class _MyAppState extends State<MyApp> {
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
-    print(platformVersion[8]);
-    if (platformVersion.contains('Android')) {
-      if (int.parse(platformVersion[8]) >= 6) {
-        print(platformVersion);
-        final res = await SimplePermissions.requestPermission(
-            Permission.ReadPhoneState);
-        print("permission request result is " + res.toString());
-      }
-    }
-    if (platformVersion.contains('iOS')) {
-         print(platformVersion);
-        final res = await SimplePermissions.requestPermission(
-            Permission.ReadPhoneState);
-        print("permission request result is " + res.toString());
-    }
+
+    await AndroidMultipleIdentifier.requestPermissions();
 
     try {
       imei = await AndroidMultipleIdentifier.imeiCode;
       serial = await AndroidMultipleIdentifier.serialCode;
       androidID = await AndroidMultipleIdentifier.androidID;
-      androidIDChar = Encoder.fromHextoString(androidID);
     } catch (e) {
       imei = 'Failed to get IMEI.';
       serial = 'Failed to get Serial Code.';
@@ -80,7 +58,6 @@ class _MyAppState extends State<MyApp> {
       _imei = imei;
       _serial = serial;
       _androidID = androidID;
-      _androidIDChar = androidIDChar;
     });
   }
 
@@ -109,12 +86,6 @@ class _MyAppState extends State<MyApp> {
               'Android ID: $_androidID\n',
               textAlign: TextAlign.center,
             ),
-            // Text('IMEI in HEX:\n ${Encoder.getHexString(_imei)}\n',textAlign:TextAlign.center,),
-            Text(
-              'Serial Code in HEX:\n ${Encoder.getHexString(_serial)}\n\nUsual size:\n ${Encoder.getHexString("0000000000000000")}',
-              textAlign: TextAlign.center,
-            ),
-            // Text('Android ID in STRING:\n $_androidIDChar\n',textAlign:TextAlign.center,),
           ],
         )),
       ),
